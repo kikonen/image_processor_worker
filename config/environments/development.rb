e9c05ffb5c6d973925fe02e7b59ced8781c76c7e
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-04-09 19:00:33 +0300

    docker access

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # https://stackoverflow.com/questions/53878453/upgraded-rails-to-6-getting-blocked-host-error
+  config.hosts.clear
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.

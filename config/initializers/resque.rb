8e0d1d218246aac9cfba091034653b2fd4856f66
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-04-10 14:23:55 +0300

    FIX env for redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 # NOTE KI https://gist.github.com/Diasporism/5631030
-Resque.redis = Redis.new(host: 'redis', port: 6379)
+Resque.redis = Redis.new(host: ENV['REDIS_HOST'], port: 6379)
